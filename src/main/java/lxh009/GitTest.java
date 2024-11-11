8d1a19e268adaa0fd5b0efeeca9cedffa35c6baa
Lucy <183083934@qq.com>
2024-11-11 22:31:14 +0800

    master commit2

--- src/main/java/lxh009/GitTest.java
+++ src/main/java/lxh009/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3333333!");
         System.out.println("hello git444!");  //好困呀！
+        System.out.println("master test！");  //合并冲突！
     }
 }
